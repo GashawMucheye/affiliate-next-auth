3a10ef46c0b89c3bfcb2943a52dc1091d5fa284b
gashaw mucheye <gashaw2533@gmail.com>
2025-03-11 00:39:03 +0200

    update navbar again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,14 +32,14 @@ export default function Navbar() {
           <Link
             href='/'
             className='text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
-            onClick={() => setMenuOpen(!menuOpen)}
+            onClick={() => setMenuOpen(menuOpen)}
           >
             Home
           </Link>
           <Link
             href='/dashboard'
             className='text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
-            onClick={() => setMenuOpen(!menuOpen)}
+            onClick={() => setMenuOpen(menuOpen)}
           >
             Dashboard
           </Link>
@@ -66,7 +66,7 @@ export default function Navbar() {
             <Link
               href='/login'
               className='bg-blue-500 text-white px-4 py-2 rounded-md'
-              onClick={() => setMenuOpen(!menuOpen)}
+              onClick={() => setMenuOpen(menuOpen)}
             >
               Login
             </Link>
@@ -74,7 +74,7 @@ export default function Navbar() {
         </div>
 
         {/* Mobile Menu Button */}
-        <button className='md:hidden' onClick={() => setMenuOpen(!menuOpen)}>
+        <button className='md:hidden' onClick={() => setMenuOpen(menuOpen)}>
           {menuOpen ? (
             <X size={28} className='text-gray-700 dark:text-gray-300' />
           ) : (
