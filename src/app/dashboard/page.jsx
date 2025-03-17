b8838b27a234b88ac267bd86beb6b3f072d9b7f8
gashaw mucheye <gashaw2533@gmail.com>
2025-03-17 02:53:12 +0200

    fixed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -118,7 +118,7 @@ export default function Dashboard() {
   return (
     <div className='min-h-screen bg-gray-100 p-6'>
       <div className='flex justify-between items-center mb-6'>
-        <h1 className='text-3xl font-bold text-center'>Admin Dashboard</h1>
+        <h1 className='text-3xl font-bold text-center'>the Admin Dashboard</h1>
       </div>
       <div className='bg-white p-6 rounded-lg shadow-md'>
         <h2 className='text-xl font-semibold mb-4'>
